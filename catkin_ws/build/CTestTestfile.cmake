b7bb7e74b4b630995bcba9c9ab189ed1173bcf10
Vishal1711 <jadhavvishal669@gmail.com>
2022-02-08 01:43:35 -0500

    First Commit

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: /home/vishal/catkin_ws/src
+# Build directory: /home/vishal/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3_simulations/turtlebot3_simulations")
+subdirs("lab21")
+subdirs("ros_basics_tutorials")
+subdirs("ros_essentials_cpp")
+subdirs("lab2")
+subdirs("turtlebot3_simulations/turtlebot3_fake")
+subdirs("turtlebot3_simulations/turtlebot3_gazebo")
---
+++ catkin_ws/build/lab21/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vishal/catkin_ws/src/lab21
+# Build directory: /home/vishal/catkin_ws/build/lab21
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
