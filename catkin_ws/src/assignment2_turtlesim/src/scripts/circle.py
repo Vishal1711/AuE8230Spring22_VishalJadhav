2a96ad52b863c88eccb385c871d42a8528635724
Vishal1711 <jadhavvishal669@gmail.com>
2022-02-10 12:41:06 -0500

    second

--- catkin_ws/src/assignment2_turtlesim/src/scripts/circle.py
+++ catkin_ws/src/assignment2_turtlesim/src/scripts/circle.py
@@ -2,31 +2,33 @@
 import rospy
 from geometry_msgs.msg import Twist
 
-def move_circle():
-    # Starts a new node
+
+def circle():
+    
+    # Creates a new node with name 'circle' and make sure it is unique node using (anonymous=True) 
     rospy.init_node('circle', anonymous=True)
+    # Publisher which will publish to topic /turtle1/cmd_vel'
     velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 
 
-    #Create twist msgs
+    #Create twist msgs which broke velocity into its linear and angular parts
     move_cmd = Twist()
     move_cmd.linear.x = 1.0
-    move_cmd.angular.z = 1.0
+    move_cmd.angular.z = 1.0      # Constant twist velocity
 
 
-    # Save current time and set publish rate
-    now = rospy.Time.now()
+    # Getting current time set publish rate
+    current_time = rospy.Time.now()
+    # go through the loop 10 times per second (10 Hz)
     rate = rospy.Rate(10)
 
-
-    while rospy.Time.now() < now + rospy.Duration.from_sec(5.7):
+    while rospy.Time.now() < current_time + rospy.Duration.from_sec(5.7):
         velocity_publisher.publish(move_cmd)
         rate.sleep()
         
         
 if __name__ == '__main__':
     try:
-        #Testing our function
-        move_circle()
+        circle()
     except rospy.ROSInterruptException:
         pass
--- catkin_ws/src/assignment2_turtlesim/src/scripts/square_openloop.py
+++ catkin_ws/src/assignment2_turtlesim/src/scripts/square_openloop.py
@@ -2,12 +2,15 @@
 import rospy
 import math
 from geometry_msgs.msg import Twist
+square = [2,2,2,2]
 
+def straight_path():
 
-def move_straight():
+    # linear velocity and distance to cover in one direction
     speed = 0.2
     distance = 2
 
+    # Since turtle moving just in x-axis
     vel_msg.linear.x = 0.2
     vel_msg.linear.y = 0
     vel_msg.linear.z = 0
@@ -15,33 +18,42 @@ def move_straight():
     vel_msg.angular.y = 0
     vel_msg.angular.z = 0
 
+    # Getting current time set 
     t0 = rospy.Time.now().to_sec()
+    # Setting the current time for distance calculus
     current_distance = 0
+    # Loop to move the turtle in an specified distance
     while(current_distance < distance):
+        # Publish the velocity
         velocity_publisher.publish(vel_msg)
+        # Takes actual time to velocity calculus
         t1 = rospy.Time.now().to_sec()
+        # Calculates distancePoseStamped
         current_distance = speed*(t1 - t0)
-
+    # After the loop, stops the robot
     vel_msg.linear.x = 0
+    # Force the robot to stop
     velocity_publisher.publish(vel_msg)
 
 def rotate():
-    angular_speed = 0.2
-    desired_angle = 90*math.pi/180
+    # angular velocity and direction
+    angular_velocity = 0.2
+    desired_angle = abs(math.radians(90))
 
+    # Since turtle rotate in only z-axis
     vel_msg.linear.x = 0
     vel_msg.linear.y = 0
     vel_msg.linear.z = 0
     vel_msg.angular.x = 0
     vel_msg.angular.y = 0
-    vel_msg.angular.z = angular_speed
+    vel_msg.angular.z = angular_velocity
 
     t0 = rospy.Time.now().to_sec()
     current_angle = 0
     while(current_angle < desired_angle):
         velocity_publisher.publish(vel_msg)
         t1 = rospy.Time.now().to_sec()
-        current_angle = angular_speed*(t1 - t0)
+        current_angle = angular_velocity*(t1 - t0)
 
     vel_msg.linear.z = 0
     velocity_publisher.publish(vel_msg)
@@ -49,12 +61,12 @@ def rotate():
 
 if __name__ == '__main__':
     try:
-        #Testing our function
+        # Testing our function
         rospy.init_node('square_openloop')
         velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 10)
         vel_msg = Twist()
-        for n in range(4):
-            move_straight()
+        for i in range(len(square)):
+            straight_path()
             rotate()
     except rospy.ROSInterruptException:
         pass
