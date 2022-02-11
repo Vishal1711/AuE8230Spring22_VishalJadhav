b09b36826793e20ad9dca6628672733fb400f3be
Vishal Jadhav <79803663+Vishal1711@users.noreply.github.com>
2022-02-10 23:02:41 -0500

    Update

--- catkin_ws/src/assignment2_turtlesim/src/scripts/square_closeloop.py
+++ catkin_ws/src/assignment2_turtlesim/src/scripts/square_closeloop.py
@@ -1,105 +1,104 @@
 #!/usr/bin/env python
+#!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 from math import pow, atan2, sqrt, pi
 
 
+class TurtleBot:
 
-def update_pose(data):
-    pose = data
-    pose.x = round(pose.x, 4)
-    pose.y = round(pose.y, 4)
+    def __init__(self):
+        # Creates a node with name 'turtlebot_controller' and make sure it is a
+        # unique node (using anonymous=True)
+        rospy.init_node('turtlebot_controller', anonymous=True)
+
+
+        # Publisher which will publish to the topic '/turtle1/cmd_vel'.
+        self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+
+        # A subscriber to the topic '/turtle1/pose'. self.update_pose is called
+        # when a message of type Pose is received.
+        self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.update_pose)
+
+        self.pose = Pose()
+        self.rate = rospy.Rate(10)
+
+    def update_pose(self, data):
+        self.pose = data
+        self.pose.x = round(self.pose.x, 4)
+        self.pose.y = round(self.pose.y, 4)
         # self.pose.theta = round(self.pose.theta, 4)
         # print('self theta -',abs(self.pose.theta))
 
-def euclidean_distance(goal_pose):
-    return sqrt(pow((goal_pose.x - pose.x), 2) + pow((goal_pose.y - pose.y), 2))
+    def euclidean_distance(self, goal_pose):
+        """Euclidean distance between current pose and the goal."""
+        return sqrt(pow((goal_pose.x - self.pose.x), 2) +
+                    pow((goal_pose.y - self.pose.y), 2))
 
-def linear_vel(goal_pose, constant=1.5):
-    return constant * euclidean_distance(goal_pose)
+    def linear_vel(self, goal_pose, constant=1.5):
+        return constant * self.euclidean_distance(goal_pose)
 
-def steering_angle(goal_pose):
-    return atan2(goal_pose.y - pose.y, goal_pose.x - pose.x)
+    def steering_angle(self, goal_pose):
+        return atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x)
 
-def angular_vel(goal_pose, constant=6):
-    return constant * (steering_angle(goal_pose) - pose.theta)
+    def angular_vel(self, goal_pose, constant=6):
+        return constant * (self.steering_angle(goal_pose) - self.pose.theta)
 
-def angular_difference(angle):
-    return (angle - pose.theta)
+    def angular_difference(self,angle):
+        return (angle - self.pose.theta)
 
-def rotate(angle,constant=1.5):
-    return constant* (angular_difference(angle))
+    def rotate(self,angle,constant=1.5):
+        return constant* (self.angular_difference(angle))
 
-    # def rotate(self,goal_pose, constant=1)
-    #     return constant * (self.angular_difference(goal_pose))
+ 
+    def move2goal(self):
+        goal_pose = Pose()
 
-def move2goal():
-    goal_pose = Pose()
+        # Get the input from the user.
+        x = [0,5,8,8,5,5]
+        y = [0,5,5,8,8,5]
+        theta = [-3*pi/4,0,pi/2,pi,-pi/2,0]
+        distance_tolerance = 0.01
+        angle_tolerance = 0.01
+        vel_msg = Twist()
+        for i in range(6):
+            goal_pose.x = x[i]
+            goal_pose.y = y[i]
 
-    # Get the input from the user.
-    x = [0,5,8,8,5,5]
-    y = [0,5,5,8,8,5]
-    theta = [-3*pi/4,0,pi/2,pi,-pi/2,0]
-    distance_tolerance = 0.01
-    angle_tolerance = 0.01
-    vel_msg = Twist()
-    for i in range(6):
-        goal_pose.x = x[i]
-        goal_pose.y = y[i]
+            while self.euclidean_distance(goal_pose) >= distance_tolerance:
+                
+                vel_msg.linear.x = 0
+                vel_msg.linear.x = self.linear_vel(goal_pose)
+                vel_msg.linear.y = 0
+                vel_msg.linear.z = 0
+                vel_msg.angular.x = 0
+                vel_msg.angular.y = 0
+                vel_msg.angular.z = self.angular_vel(goal_pose)
+                self.velocity_publisher.publish(vel_msg)
+                self.rate.sleep()
 
-        while euclidean_distance(goal_pose) >= distance_tolerance:
-            # print('moving in here',self.euclidean_distance(goal_pose))
-            vel_msg.linear.x = 0
-            vel_msg.linear.x = linear_vel(goal_pose)
-            vel_msg.linear.y = 0
-            vel_msg.linear.z = 0
-            vel_msg.angular.x = 0
-            vel_msg.angular.y = 0
-            vel_msg.angular.z = angular_vel(goal_pose)
-            velocity_publisher.publish(vel_msg)
-            rate.sleep()
+            while abs(self.angular_difference(theta[i])) >= angle_tolerance:
+                
+                vel_msg.linear.x = 0
+                vel_msg.linear.y = 0
+                vel_msg.linear.z = 0
+                vel_msg.angular.x = 0
+                vel_msg.angular.y = 0
+                vel_msg.angular.z = self.rotate(theta[i])
+                self.velocity_publisher.publish(vel_msg)
+                self.rate.sleep()
 
-        while abs(angular_difference(theta[i])) >= angle_tolerance:
-            # print('rotating in here',angular_difference(theta[i]))
-            vel_msg.linear.x = 0
-            vel_msg.linear.y = 0
-            vel_msg.linear.z = 0
-            vel_msg.angular.x = 0
-            vel_msg.angular.y = 0
-            vel_msg.angular.z = rotate(theta[i])
-            velocity_publisher.publish(vel_msg)
-            rate.sleep()
+        vel_msg.linear.x = 0
+        vel_msg.angular.z = 0
+        self.velocity_publisher.publish(vel_msg)
 
-    vel_msg.linear.x = 0
-    vel_msg.angular.z = 0
-    velocity_publisher.publish(vel_msg)
-
-        
-    rospy.spin()
+        # If we press control + C, the node will stop.
+        rospy.spin()
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('turtlebot_controller', anonymous=True)
-
-        
-        velocity_publisher = rospy.Publisher('/turtle1/cmd_vel',Twist, queue_size=10)
-
-       
-    
-        pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, update_pose)
-        pose = Pose()
-        rate = rospy.Rate(10)
-        data = pose
-        update_pose(data)
-        move2goal()
-        euclidean_distance(goal_pose)
-        linear_vel(goal_pose, constant=1.5)
-        steering_angle(goal_pose)
-        angular_vel(goal_pose, constant=6)
-        angular_difference(angle)
-        rotate(angle,constant=1.5)
-        
-
+        x = TurtleBot()
+        x.move2goal()
     except rospy.ROSInterruptException:
         pass
